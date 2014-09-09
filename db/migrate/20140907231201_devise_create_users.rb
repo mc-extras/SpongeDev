e810aa4e497e084b9adc2af8720785d8cf8481f4
Jake <jake0oo0andminecraft@gmail.com>
2014-09-08 22:53:41 -0500

    Add migration

--- db/migrate/20140907231201_devise_create_users.rb
+++ db/migrate/20140907231201_devise_create_users.rb
@@ -4,9 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
-      t.string :username, null: false, default: ""
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907232064) do
+ActiveRecord::Schema.define(version: 20140909035208) do
 
   create_table "forem_categories", force: true do |t|
     t.string   "name",       null: false
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 20140907232064) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",               null: false
     t.string   "encrypted_password",     default: "",               null: false
-    t.string   "username",               default: "",               null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -125,9 +124,11 @@ ActiveRecord::Schema.define(version: 20140907232064) do
     t.boolean  "forem_admin",            default: false
     t.string   "forem_state",            default: "pending_review"
     t.boolean  "forem_auto_subscribe",   default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
