8e6a2ff9faa34ff2e66c41d158ea16988c2dd3c8
Jake <jake0oo0andminecraft@gmail.com>
2014-09-09 16:28:55 -0500

    Add active classes to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active(page)
+    if params[:controller].match(/^forem/) and page == "forums"
+      return "active"
+    end 
+    return params[:controller] == page ? "active" : ""
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909035208) do
+ActiveRecord::Schema.define(version: 20140909204643) do
 
   create_table "forem_categories", force: true do |t|
     t.string   "name",       null: false
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20140909035208) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "reply_to_id"
-    t.string   "state",       default: "pending_review"
+    t.string   "state",       default: "approved"
     t.boolean  "notified",    default: false
   end
 
@@ -79,11 +79,11 @@ ActiveRecord::Schema.define(version: 20140909035208) do
     t.string   "subject"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "locked",       default: false,            null: false
+    t.boolean  "locked",       default: false,      null: false
     t.boolean  "pinned",       default: false
     t.boolean  "hidden",       default: false
     t.datetime "last_post_at"
-    t.string   "state",        default: "pending_review"
+    t.string   "state",        default: "approved"
     t.integer  "views_count",  default: 0
     t.string   "slug"
   end
@@ -109,12 +109,12 @@ ActiveRecord::Schema.define(version: 20140909035208) do
   add_index "forem_views", ["viewable_id"], name: "index_forem_views_on_viewable_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",               null: false
-    t.string   "encrypted_password",     default: "",               null: false
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 20140909035208) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "forem_admin",            default: false
-    t.string   "forem_state",            default: "pending_review"
+    t.string   "forem_state",            default: "approved"
     t.boolean  "forem_auto_subscribe",   default: false
     t.string   "username"
   end
