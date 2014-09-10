2ec28b0f88c4799dbcb84d889e9602d9702412bd
Jake <jake0oo0andminecraft@gmail.com>
2014-09-10 16:45:15 -0500

    Add profile links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
-  mount Forem::Engine, :at => "/forums"
-  get '/resources/terms' => "resources#terms", as: "terms"
-  get '/resources/rules' => "resources#rules", as: "rules"
+  root 'home#index'
+  mount Forem::Engine, :at => '/forums'
+  get '/resources/terms' => 'resources#terms', as: 'terms'
+  get '/resources/rules' => 'resources#rules', as: 'rules'
   resources :revisions
+  get '/user/:user' => 'application#profile', as: 'profile'
 end
