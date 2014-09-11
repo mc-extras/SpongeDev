bf173ce90b0d64985b721431d00e4f6fb2036d0d
Jake <jake0oo0andminecraft@gmail.com>
2014-09-11 18:58:09 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ announcements = Forem::Category.create(:name => "Meta")
 Forem::Forum.create(:category => announcements, :name => "News and Announcements", :description => "Project announcements from Sponge staff.")
 Forem::Forum.create(:category => announcements, :name => "Feedback", :description => "A place for the community to give feedback on the Sponge project.")
 
-Forem::Category.create(:name => "General")
-Forem::Forum.create(:category => Forem::Category.first, :name => "General Discussion", :description => "Discussion about anything related to Sponge.")
+general = Forem::Category.create(:name => "General")
+Forem::Forum.create(:category => general, :name => "General Discussion", :description => "Discussion about anything related to Sponge.")
 
 plugins = Forem::Category.create(:name => "Development")
 Forem::Forum.create(:category => plugins, :name => "Sponge Development", :description => "Discussion related to the development of Sponge.")
