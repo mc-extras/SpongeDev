205e795ef03960f53e6d5896993e8ee9d0ff350e
riksolo <rikberkelder@gmail.com>
2014-09-14 17:41:02 +0200

    github didnt sync yours with mine,
    IF THIS ENTERED THE PULL REQUEST, DONT MERGE THIS ONE! ONLY THE OTHERS

---
+++ app/models/plugin.rb
@@ -0,0 +1,9 @@
+class Plugin < ActiveRecord::Base
+  acts_as_taggable
+  has_many :authors
+  belongs_to :user
+
+  def to_param
+    "#{id} #{name}"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
   def forem_name
     username
@@ -11,4 +12,11 @@ class User < ActiveRecord::Base
   def forem_email
     email
   end
+
+  has_many :infractions
+  has_many :sent_messages, class_name: 'Message', inverse_of: 'creator'
+  belongs_to :recieved_messages, class_name: 'Message', inverse_of: 'recipients'
+  has_many :authors
+  has_many :plugins
+
 end
