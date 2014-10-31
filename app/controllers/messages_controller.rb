516bc6b3b3199d0607b2d9b4b0b8fcf593b83e02
Jake <jake0oo0andminecraft@gmail.com>
2014-10-30 21:45:22 -0500

    Working on messages more

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,5 +6,29 @@ class MessagesController < ApplicationController
   end
 
   def show
+    unless @conversation = Mailboxer::Conversation.find(params[:id])
+      return redirect_to messages_path, :notice => "That conversation does not exist."
+    end
+    unless @conversation.is_participant?(current_user)
+      return redirect_to messages_path, :notice => "You do not have permission to view that conversation."
+    end
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = Mailboxer::Message.new(message_params)
+    if @message.conversation_id
+      @conversation = Mailboxer::Conversation.find(@message.conversation_id)
+      unless @conversation.is_participant?(current_user)
+        return redirect_to messages_path, :notice => "You do not have permission to view that conversation."
+      end
+      receipt = current_user.reply_to_conversation(@conversation, @message.body, nil, true, true, @message.attachment)
+    end
+    return redirect_to message_path(receipt.conversation), :notice => "Message created."
+  end
+
+  private
+  def message_params
+    params.require(:mailboxer_message).permit(:body, :conversation_id)
   end
 end
