5390746fa8b8462a6af982cf073bbde8217af753
Jake <jake0oo0andminecraft@gmail.com>
2014-11-02 07:43:04 -0600

    Add helper method to get announcements, to slow to use in production

---
+++ app/helpers/news_helper.rb
@@ -0,0 +1,24 @@
+module NewsHelper
+  def get_announcements
+    response = HTTParty.get("http://forums.spongepowered.org/c/announcements.json")
+    puts response
+    json = JSON.parse(response.body)
+    puts json
+    topics = json['topic_list']['topics']
+    announcements = Array.new
+    topics.each do |topic|
+      title = topic['title']
+      posted_at = topic['created_at']
+      id = topic['id']
+      next if not id
+      url = "http://forums.spongepowered.org/t/#{id}"
+      puts url + '.json'
+      thread_json = JSON.parse(HTTParty.get(url + '.json').body)
+      thread = thread_json['post_stream']['posts'][0]
+      author = thread['username']
+      text = ActionView::Base.full_sanitizer.sanitize(thread['cooked'])
+      topics << {title: title, posted_at: posted_at, url: url, author: author, text: text}
+    end
+    topics
+  end
+end
