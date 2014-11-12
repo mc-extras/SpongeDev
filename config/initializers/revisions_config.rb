c47a1f8cd3af3517c876332b478246a3f5beb29c
Jake <jake0oo0andminecraft@gmail.com>
2014-11-12 16:35:24 -0600

    Update repo name to SpongeDev in revisions

--- config/initializers/revisions_config.rb
+++ config/initializers/revisions_config.rb
@@ -1,2 +1,2 @@
-GithubRevisions.repositories = [{:user => 'Jake0oo0', :name => 'Spongy', :friendly => 'Web'}, {:user => 'SpongePowered', :name => 'Sponge', :friendly => 'Sponge'}, {:user => 'SpongePowered', :name => 'SpongeAPI', :friendly => 'Sponge API'}]
+GithubRevisions.repositories = [{:user => 'Jake0oo0', :name => 'SpongeDev', :friendly => 'Web'}, {:user => 'SpongePowered', :name => 'Sponge', :friendly => 'Sponge'}, {:user => 'SpongePowered', :name => 'SpongeAPI', :friendly => 'Sponge API'}]
 # GithubRevisions.oauth = ''
